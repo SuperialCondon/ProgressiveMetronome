0bbede7b3efd79a255ed6650a9b93e8fee2a94eb
Michael <superialcondon@gmail.com>
2020-04-03 12:42:42 -0600

    Initial commit

---
+++ ProgressiveMetronome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProgressiveMetronome
+//
+//  Created by Michael Condon on 4/2/20.
+//  Copyright © 2020 Michael Condon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World, I am an app that might do something important someday.")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
